30accb3a7db07a1c77e8683bc34a7005992c5282
sabyathak <sabyathak22@gmail.com>
2019-07-11 16:34:27 -0700

    Initial Commit

---
+++ Emojinal/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Emojinal
+//
+//  Created by Apple on 7/11/19.
+//  Copyright © 2019 sabyatha. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func showMessage(sender: UIButton) {
+        
+    }
+}
+
