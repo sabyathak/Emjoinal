99f1fba3cdd873ec0c51c4e5811ad6ca8bf60e99
sabyathak <sabyathak22@gmail.com>
2019-07-12 09:31:33 -0700

    Second Commit

--- Emojinal/ViewController.swift
+++ Emojinal/ViewController.swift
@@ -16,6 +16,13 @@ class ViewController: UIViewController {
     }
 
     @IBAction func showMessage(sender: UIButton) {
+        let alertController = UIAlertController(title : "title", message : "title", preferredStyle: UIAlertController.Style.alert)
+        
+        alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+        
+        present(alertController, animated: true, completion: nil)
+        
+        
         
     }
 }
